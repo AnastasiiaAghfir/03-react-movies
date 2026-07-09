f16c2a88a686d1d356828df1f14892dd488759fa
Anastasiia_Aghfir <anastasiia.aghfir@gmail.com>
2026-07-09 14:47:26 +0200

    fixed after review

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
+import type {Movie} from '../types/movie'
 
-export default async function fetchMovies(query: string) {
-    const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+interface MovieResponse {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+}
+
+export default async function fetchMovies(query: string): Promise<Movie[]> {
+    const response = await axios.get<MovieResponse>('https://api.themoviedb.org/3/search/movie', {
         params: {
-          query: query,
-          api_key: import.meta.env.VITE_TMDB_TOKEN,
+            query,
         },
+        headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+        }
     })
     return response.data.results;
 }
